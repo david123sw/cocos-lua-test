dc4bfc5c668b7436abb60492468fe361f9e1831e
david123sw <shenwei.123sw@gmail.com>
2018-06-26 23:14:49 +0800

    dragonbones win32&android

---
+++ 3rd-support/Dragonbones-2dx-lua/DragonBoneCreator.lua
@@ -0,0 +1,68 @@
+--region *.lua
+--Date
+--此文件由[BabeLua]插件自动生成
+require ("app/libs/dragonbones")
+local gt = cc.exports.gt
+local DragonBoneCreator = class("DragonBoneCreator")
+
+DragonBoneCreator.DEFAULT_SCALE = 0.618
+DragonBoneCreator.DEFUALT_POSITION = cc.p(0, 0)
+DragonBoneCreator.DEFUALT_ARMATURE_NAME = "armatureDisplay"
+DragonBoneCreator.cacheDBObjs = DragonBoneCreator.cacheDBObjs or {}
+
+--[[----        self._armatureDisplay:bindDragonEventListener(handler(self,self._animationEventHandler))
+----        self._armatureDisplay:addDragonEventType("start")
+----        self._armatureDisplay:addDragonEventType("loopComplete")
+----        self._armatureDisplay:addDragonEventType("complete")
+----function EntryMainScene:_animationEventHandler(typename, name, eventobj, obj)
+----    gt.log(eventobj:AnimationState():getName() .. "," .. typename .. "," .. name)
+----end
+]]
+
+--dbData.skeDataPath
+--dbData.texDataPath
+--dbData.armatureName
+--dbData.animationName
+--dbData.targetNode
+function DragonBoneCreator:ctor(dbData)
+    gt.log("*************************DragonBoneCreator:ctor*************************")
+    if nil == dbData.targetNode then
+        gt.log("DB target node not found, return")
+        return
+    end
+
+    local exist = DragonBoneCreator.cacheDBObjs[dbData.targetNode]
+    if nil ~= exist then
+        gt.log("DB target node already added, return")
+        return
+    end
+
+    local dbConfig = {}
+    dbConfig._dbFactory = db.CCFactory:getInstance()
+    dbConfig._dragonBonesData = dbConfig._dbFactory:loadDragonBonesData(dbData.skeDataPath)
+    dbConfig._dbFactory:loadTextureAtlasData(dbData.texDataPath)
+    dbConfig._armatureDisplay = dbConfig._dbFactory:buildArmatureDisplay(dbData.armatureName)
+    dbConfig._armatureDisplay:setPosition(DragonBoneCreator.DEFUALT_POSITION)
+    dbConfig._armatureDisplay:setScale(DragonBoneCreator.DEFAULT_SCALE)
+    dbConfig._armatureDisplay:setName(DragonBoneCreator.DEFUALT_ARMATURE_NAME)
+    dbConfig._armatureDisplay:getAnimation():play(dbData.animationName)
+    dbData.targetNode:addChild(dbConfig._armatureDisplay)
+    dbConfig.dbData = dbData
+
+    DragonBoneCreator.cacheDBObjs[dbData.targetNode] = dbConfig
+end
+
+function DragonBoneCreator:disposeDB(targetNode)
+    gt.log("*************************DragonBoneCreator:disposeDB*************************")
+    if nil == targetNode or nil == DragonBoneCreator.cacheDBObjs[targetNode] then
+        return
+    end
+
+    local dbConfig = DragonBoneCreator.cacheDBObjs[targetNode]
+    dbConfig._armatureDisplay:getAnimation():stop(dbConfig.dbData.animationName)
+    dbConfig._armatureDisplay:removeFromParent()
+    DragonBoneCreator.cacheDBObjs[targetNode] = nil
+end
+
+return DragonBoneCreator
+--endregion
